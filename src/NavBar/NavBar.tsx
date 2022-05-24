ae7ca46f2fdbb04f0c0c7029804c05f81750bba5
Christopher Venczel <christopher.venczel@outlook.com>
2022-05-24 17:05:18 -0400

    test

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -2,31 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './NavBar.css';
 
 // This contains the entire website
-const NavBar = (): JSX.Element => {
-
-  const [isMobile, setIsMobile] = useState(false);
-
-  window.addEventListener('resize', function (event) {
-    // Check if on mobile
-    let mobileCheck = false;
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
-      window.innerWidth < 700) {
-      mobileCheck = true;
-      console.log("here");
-    }
-    if (!isMobile && mobileCheck) {
-      setIsMobile(true);
-    } else if (isMobile && !mobileCheck) {
-      setIsMobile(false);
-    }
-  }, true);
-
-
-
+const NavBar = (props): JSX.Element => {
   return (
     <>
       {/* Desktop Nav */}
-      {!isMobile && (
+      {!props.isMobile && (
         <div className="desktopNav">
           <div className="container">
             <a className="logo"><h1>Chris Venczel</h1></a>
@@ -37,23 +17,23 @@ const NavBar = (): JSX.Element => {
         </div>
       )}
       {/* Mobile Nav */}
-      {isMobile && (
+      {props.isMobile && (
         <div className="mobileNav">
           <div className="container">
             <table>
               <tbody>
                 <tr>
                   <td>
-                    <a className="logo"><h1>Chris Venczel</h1></a>
+                    <a href="page.html#homeA" className="logo"><h1>Chris Venczel</h1></a>
                   </td>
                   <td>
-                    <a className="navlink">projects</a>
+                    <a href="page.html#projectsA" className="navlink">projects</a>
                   </td>
                   <td>
-                    <a className="navlink">about</a>
+                    <a href="page.html#aboutA" className="navlink">about</a>
                   </td>
                   <td>
-                    <a className="navlink" style={{ marginRight: 0 }}>contact</a>
+                    <a href="page.html#contactA" className="navlink" style={{ marginRight: 0 }}>contact</a>
                   </td>
                 </tr>
               </tbody>
--- src/Website/Website.tsx
+++ src/Website/Website.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Website.css';
 // @ts-ignore
 import Earth from '../Earth/Earth.tsx';
@@ -7,18 +7,89 @@ import NavBar from '../NavBar/NavBar.tsx';
 
 // This contains the entire website
 const Website = (): JSX.Element => {
+
+  const [viewToProjects, setViewToProjects] = useState(true);
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    checkIfMobile();
+  }, []);
+
+  window.addEventListener('resize', () => {
+    checkIfMobile();
+  }, true);
+
+  // Check if on mobile
+  const checkIfMobile = () => {
+    let mobileCheck = false;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ||
+      window.innerWidth < 700) {
+      mobileCheck = true;
+    }
+    if (!isMobile && mobileCheck) {
+      setIsMobile(true);
+    } else if (isMobile && !mobileCheck) {
+      setIsMobile(false);
+    }
+  }
+
+  window.onscroll = (e) => {
+    // Sections - 1. Home, 2. Projects, 3. About, 4. Contact
+    const sectionHeights = Array.from(document.getElementsByClassName("section")).map(section => {
+      const rect = section.getBoundingClientRect();
+      return rect.bottom < window.innerHeight * 0.3;
+    });
+    const sectionIn = sectionHeights.indexOf(false);
+    const sections = [
+      Array.from(document.getElementsByClassName("logo"))[0],
+      Array.from(document.getElementsByClassName("navlink"))[0],
+      Array.from(document.getElementsByClassName("navlink"))[1],
+      Array.from(document.getElementsByClassName("navlink"))[2]
+    ];
+    const currentSection = sections[sectionIn];
+    for (let i = 0; i < sections.length; i++) {
+      if (-i != 0 && sections[i] === currentSection) {
+        sections[i].classList.add("aHover");
+      } else {
+        sections[i].classList.remove("aHover");
+      }
+    }
+    if (document.documentElement.scrollTop > 0) {
+      setViewToProjects(false);
+    } else {
+      setViewToProjects(true);
+    }
+  }
+
   return (
     <div className="main">
-      <NavBar />
+      <NavBar
+        isMobile={isMobile}
+      />
       {/*<Earth />*/}
-      <div className="homePage">
+      <div className="section homeSection">
 
       </div>
-      <div id="footerText">
-        <span>to projects ⤵</span>
+      <div className="section projectsSection">
+
       </div>
+      <div className="section aboutSection">
+
+      </div>
+      <div className="section contactSection">
+
+      </div>
+      <div id="copyright">
+        Copyright © 2022 Christopher Venczel.&nbsp;&nbsp;
+        MIT Licence
+      </div>
+      {viewToProjects && (
+        <div id="footerText">
+          <span>to projects ⤵</span>
+        </div>
+      )}
       <div id="footerShadow"></div>
-      <div id="footerSpacer"></div>
     </div>
   );
 }
