b399aa2b8a3ea538e43eeeeb1e2ff92c556b182c
Christopher Venczel <christopher.venczel@outlook.com>
2022-06-02 01:31:45 -0400

    Test

--- src/components/Mandelbrot/Mandelbrot.tsx
+++ src/components/Mandelbrot/Mandelbrot.tsx
@@ -22,10 +22,11 @@ const Mandelbrot: FC<MandelbrotProps> = (props) => {
     const imageData = ctx.getImageData(0, 0, canvas.current.width, canvas.current.height);
     const data = imageData.data;
     for (var i = 0; i < data.length; i += 4) {
-        data[i]     = 255 - data[i];     // red
-        data[i + 1] = 255 - data[i + 1]; // green
-        data[i + 2] = 255 - data[i + 2]; // blue
+        data[i]     = 10;     // red
+        data[i + 1] = 10; // green
+        data[i + 2] = 10; // blue
     }
+    console.log(data, canvas.current, ctx);
     ctx.putImageData(imageData, 0, 0);
   }, [ctx, canvas.current]);
 
